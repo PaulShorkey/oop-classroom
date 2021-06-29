fce6938d4459a6ec3f71d8b04977a005bac8eda9
Paul Shorkey-Chacon <paul@Pauls-MacBook-Pro.local>
2021-06-29 16:14:06 -0400

    Finished Desk and Computer

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,19 @@
 class Desk {
-  
+  constructor (length, width, height, isWhiteboard) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    this.isWhiteboard = isWhiteboard;
+    this.content = '';
+  }
+  write(inputString){
+    this.content = this.content + inputString;
+  }
+  wipe(){
+    if (this.isWhiteboard){
+        this.content = '';
+    }
+  }
 }
 
 module.exports = Desk
--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,18 @@
 class Marker {
-  
+  constructor(size, color, remainingInk){
+    this.size = size;
+    this.color = color;
+    this.remainingInk = remainingInk;
+  }
+
+  write (stringToBeWritten) { 
+    let numChars = stringToBeWritten.split(' ').length;
+    this.remainingInk -= numChars;
+    
+    let upToIndex = stringToBeWritten.length - numChars 
+    
+    return stringAbleToBeWritten.substring(0,upToIndex);
+  }
 }
 
 module.exports = Marker
